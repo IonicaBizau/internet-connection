c6b55edee65b7d1091a3eaa4d461abeeadd243e6
Ionica Bizau <bizauionica@yahoo.com>
2013-08-15 09:37:41 +0300

    Add repo files.

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#############################
+# Check Internet Connectivity
+#############################
+# A small script that plays a beep when there is no Internet connection
+#
+# Follow me on Github :: github.com/IonicaBizau
+# ==========================
+
+for (( ; ; ))
+do
+    sleep $1
+    echo " * Checking "
+    wget --spider http://example.com
+    if [ "$?" != 0 ]; then
+        echo "+-----------------------------------------------+"
+        echo "+    !!!!!    No Internet connection    !!!!!   +"
+        echo "+-----------------------------------------------+"
+        mplayer beep-1.wav
+    fi
+done
